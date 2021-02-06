f594dd52bb9231ff0eaeda787baf8b6c89fb0179
fasbrothers <nigmatovsanjar00@gmail.com>
2021-02-06 13:43:21 +0500

    made some changes

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" alt="logo"></img>
+            <img src="https://gradle.org/images/gradle-400x400.png" alt="avatar" />
+            <h1>This is nav</h1>
+        </div >
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqwQD2rx-8NCQvbDxBYwojC8u1ip6ZGzA",
+    authDomain: "netflix-clone-b21c3.firebaseapp.com",
+    projectId: "netflix-clone-b21c3",
+    storageBucket: "netflix-clone-b21c3.appspot.com",
+    messagingSenderId: "471039819096",
+    appId: "1:471039819096:web:a4136a77c2d77665a1392d"
+};
