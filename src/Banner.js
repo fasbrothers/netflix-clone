15bbce628f1122cffa7175c70933b3abfa03ea23
fasbrothers <nigmatovsanjar00@gmail.com>
2021-02-07 23:37:32 +0500

    made some changes

--- src/Banner.js
+++ src/Banner.js
@@ -31,16 +31,14 @@ function Banner() {
         }}>
             <div className="banner__contents">
                 <h1 className="banner__title">
-                    Movie Name
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h6 className="banner__description">{truncate
-                    (`This is a test description This is a test description This is a test descriptionThis is a test descriptionThis
-                is a test description This is a test descriptionThis is a test
-                descriptionThis is a test descriptionThis is a test description`, 200)}</h6>
+                    (movie?.overview, 150)}</h6>
             </div>
             <div className="banner--fadeBotton" />
         </header >
